507cc546bc3e058dc0efa8193d89fc4ee028e38b
DDstar1 <abhuluimendestiny@gmail.com>
2024-08-12 06:23:20 +0100

    'dvds'

--- src/app/api/migrate/route.js
+++ src/app/api/migrate/route.js
@@ -1,10 +1,14 @@
 import { migrate } from "../migrations";
+import { NextRequest, NextResponse } from "next/server";
 
-export default async function handler(req, res) {
+export async function GET(req, res) {
   try {
     await migrate();
-    res.status(200).json({ message: "Migration successful" });
+    return NextResponse.json({ message: "Migration successful" });
   } catch (error) {
-    res.status(500).json({ error: "Migration failed", details: error.message });
+    return NextResponse.json({
+      error: "Migration failed",
+      details: error.message,
+    });
   }
 }
